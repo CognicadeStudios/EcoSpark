8435b24196a7a3d140fccc629cd6bfedd7393c96
ArushNo1 <arushbodla@gmail.com>
2024-09-24 22:24:30 -0400

    Skill Tree Beginnings
    Added some files and started working on the skill tree, still looking into events and stuff.

--- Assets/Scripts/ResearchManager.cs
+++ Assets/Scripts/ResearchManager.cs
@@ -1,67 +1,35 @@
+using CodeMonkey;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using Unity.Burst.CompilerServices;
-using UnityEditor.Rendering.Universal;
 
+/// <summary>
+/// / Controls the level of each upgrade, and answers queries about what things are possible
+/// </summary>
 public class ResearchManager : MonoBehaviour
-{
-    public ResearchPointHolder rp;
-    //public int[] researchCosts;
-    //public int[] researchLevels;
-    //public float[] timeLeft;
-    //public float[] researchTimes;
-    //public Slider[] researchSliders;
-    //public Button[] startButtons;
-    public Upgrade[] upgrades;
-    int numUpgrades;
+{    
     
     // Start is called before the first frame update
     void Start()
     {
-        rp = FindObjectOfType<ResearchPointHolder>();
-        numUpgrades = upgrades.Length;
-
-        Debug.Log("NumUpgrades: " + numUpgrades);
-
-        for (int i = 0; i < numUpgrades; i++)
-        {
-            upgrades[i].ID = i;
-            upgrades[i].slider.maxValue = 1.0f;
-            upgrades[i].slider.value = 0f;
-            int ID = i;
-            upgrades[i].button.onClick.AddListener(() => StartResearch(ID));
-        }
-    }
-
-    void StartResearch(int ind)
-    {
-        Debug.Log("ind:" + ind);
-        if (upgrades[ind].timeLeft > 0f){
-            Debug.Log("Research Already Running");
-            return;
-        }
-        upgrades[ind].timeLeft = upgrades[ind].upgradeTime;
+        InvokeRepeating("OneSecond", 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < numUpgrades; i++)
-        {
-            if (upgrades[i].timeLeft <= 0f)
-            {
-                continue;
-            }
-            upgrades[i].timeLeft -= Time.deltaTime;
-            upgrades[i].slider.value = 1f - (upgrades[i].timeLeft / upgrades[i].upgradeTime);
-            if (upgrades[i].timeLeft < 0f)
-            {
-                upgrades[i].timeLeft = 0f;
-                upgrades[i].levelUp();
-            }
-        }
+    }
+
+    void OneSecond()
+    {
+        int x = Upgrade.solar.sun;
+        Debug.Log("sun: " + x);
+        Upgrade.solar.Update();
+    }
+
+    public void UnlockUpgrade(Upgrade upgrade)
+    {
+        Debug.Log("Upgrade Unlocking");
+        upgrade.LevelUp();
     }
 }
