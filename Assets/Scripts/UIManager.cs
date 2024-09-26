f71d8828e1a1ca0cf14910fa521f848837f3279a
ashmithry <ashuyaramada@gmail.com>
2024-09-25 22:45:07 -0400

    fancy ui started

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject buildMenuPanel, resButton, mailButton;
+    private bool buildMenuOpen = false;
+
+    public void ToggleBuildMenu()
+    { 
+        buildMenuOpen = !buildMenuOpen;
+        if (buildMenuOpen) { OpenBuildMenu(); } else { CloseBuildMenu(); }
+    }
+    public void OpenBuildMenu()
+    {
+        LeanTween.scale(buildMenuPanel, new Vector3(1f, 1f, 1f), 0.4f).setEase(LeanTweenType.easeOutExpo);
+        
+    }
+
+    public void CloseBuildMenu()
+    {
+        LeanTween.scale(buildMenuPanel, new Vector3(0f, 1f, 1f), 0.4f).setEase(LeanTweenType.easeOutExpo).setOnComplete(delegate ()
+        {
+            buildMenuPanel.transform.localScale = new Vector3(0f, 1f, 1f);
+        });
+    }
+
+    public static void CloseButton(GameObject g) {
+        LeanTween.scale(g, new Vector3(0f, 0f, 0f), 0.1f).setEase(LeanTweenType.easeOutElastic);
+    }
+
+    public static void OpenButton(GameObject g)
+    {
+        LeanTween.scale(g, new Vector3(1f, 1f, 1f), 0.1f).setEase(LeanTweenType.easeOutElastic);
+    }
+}
---
+++ Assets/Scripts/UITweener.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UITweener : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float tweenTime = 1f;
+    public LeanTweenType ease = LeanTweenType.easeOutElastic;
+    public Vector3 scaleTo;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        LeanTween.cancel(gameObject);
+        LeanTween.scale(gameObject, scaleTo, 1f).setEase(ease);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.cancel(gameObject);
+        LeanTween.scale(gameObject, new Vector3(1f,1f,1f), 1f).setEase(ease);
+    }
+}
