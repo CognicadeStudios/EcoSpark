61ba66333ec402cd3269123ec5ef8cd4e32bc332
ashmithry <ashuyaramada@gmail.com>
2024-10-21 20:48:34 -0400

    final build push

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -192,11 +192,11 @@ public class UIManager : MonoBehaviour
 
     public string GenerateStatsText()
     {
-        return "Total Energy Generated:\t\t" + Mathf.Round(GameManager.Instance.CityEnergy) + "\n\n\n" 
+        return "Total Energy Generated:\t\t" + FormatNumberAsK((int)GameManager.Instance.CityEnergy) + "\n\n\n" 
                     + "Research Points:\t\t\t" +  Mathf.Round(GameManager.Instance.ResearchPoints) + "\n\n\n"
                     + "Public Approval:\t\t\t" +  Mathf.Round(GameManager.Instance.PublicApproval) + "\n\n\n"
                     + "Enviornmental Score:\t\t" +  Mathf.Round(GameManager.Instance.EcoScore) + "\n\n\n"
-                    + "Total Money Earned:\t\t" +  Mathf.Round(GameManager.Instance.Money);
+                    + "Total Money Earned:\t\t" +  FormatNumberAsK((int)GameManager.Instance.Money);
     }
 
     public void ToggleUIScreen(GameObject screen)
